ef83e3f7df1119812fcce95d499e18758508635b
Dominic <18015@totton.ac.uk>
2022-09-21 00:30:08 +0100

    push

--- Phone Project/Assets/Scripts/TestClick.cs
+++ Phone Project/Assets/Scripts/TestClick.cs
@@ -5,21 +5,36 @@ public class TestClick : MonoBehaviour
 {
     TMP_Text text;
     int count = 0;
+    int upgradeCount = 0;
+    int cost = 100;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         text = GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         text.text = count.ToString();
     }
 
+    private void CheckUpgrade()
+    {
+        if (count >= cost)
+        {
+            upgradeCount++;
+
+        }
+    }
+
+    private void Upgrade()
+    {
+
+    }
+
+
     public void Increment()
     {
-        count++;
+        count *= upgradeCount;
     }
 }
